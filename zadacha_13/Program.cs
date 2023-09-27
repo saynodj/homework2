8bc06ca9328c3e94dfbc9ccb78d66038abc877a9
sergey <s.novak1990@mail.ru>
2023-09-27 23:43:44 +0300

    zadacha 13 reshenie

--- zadacha_13/Program.cs
+++ zadacha_13/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+Console.WriteLine("Задача № 13 ");
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else 
+{
+  Console.WriteLine("-> третьей цифры нет");
+}
