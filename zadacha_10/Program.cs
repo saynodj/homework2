2eba696c5123fbd9a1376b42ac84c2d023bfcc7c
sergey <s.novak1990@mail.ru>
2023-09-27 23:41:33 +0300

    zadacha 10 formatirovanie

--- zadacha_10/Program.cs
+++ zadacha_10/Program.cs
@@ -8,4 +8,4 @@ Console.WriteLine("Задача № 10 ");
 Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+Console.WriteLine("вторая цифра этого числа -> " + stringNumber[1]);
